b5055918650f7225f15b3c77deff1fae9c56312e
dpavam <hhz161@qmul.ac.uk>
2024-11-12 20:09:11 +0000

    feat: adds logger

--- export_phenodigm_tables.sh
+++ export_phenodigm_tables.sh
@@ -6,6 +6,11 @@ usage() {
     exit 1  
 }
 
+# Makeshift Logger
+log() {
+    echo "$(date +'%Y-%m-%d %H:%M:%S') - $1"
+}
+
 # Parse flags
 while [[ "$#" -gt 0 ]]; do
     case $1 in
@@ -13,14 +18,14 @@ while [[ "$#" -gt 0 ]]; do
     --db) DB_PATH="$2"; shift ;;
     --code) CODE_PATH="$2"; shift ;;
     --output_dir) OUTPUT_DIR="$2"; shift ;;
-    *) echo "Unknown parameter: $1"; usage ;;
+    *) log "Unknown parameter: $1"; usage ;;
     esac
     shift
 done
 
 # Check for mandatory arguments
 if [[ -z "$VENV_PATH" || -z "$DB_PATH" || -z "$CODE_PATH" || -z "$OUTPUT_DIR" ]]; then
-    echo "Error: Missing required arguments."
+    log "Error: Missing required arguments."
     usage
 fi
 
@@ -30,36 +35,36 @@ module load pigz/2.6
 # 1. Decompress the db if codeits compressed
 # Conditional to check 
 if [[ "$DB_PATH" == *.tar.gz ]]; then 
-    echo "Decompressing files..."
+    log "Decompressing files..."
     tar --use-compress-program=pigz -xvf "$DB_PATH"
     DB_PATH="${DB_PATH%.tar.gz}"
 else
-    echo "No decompression needed, the file does not have a .tar.gz extension."
+    log "No decompression needed, the file does not have a .tar.gz extension."
 fi
 
 # 2. Activate the virutal environment
-echo "Activating virtual environment..."
+log "Activating virtual environment..."
 module load python/3.8.5
-source "$VENV_PATH/bin/activate" || { echo "Failed to activate virtual environment"; exit 1; }
+source "$VENV_PATH/bin/activate" || { log "Failed to activate virtual environment"; exit 1; }
 
 #3. Create output directory:
 mkdir -p "$OUTPUT_DIR"
 
 # 4. a. Extract all of the required tables
-echo "Extracting tables..."
+log "Extracting tables..."
 tables=("model" "model_genotype" "disease" "disease_gene_mapping" "gene_gene_mapping")
 for i in "${!tables[@]}"; do
     table="${tables[$i]}"
     if python3 "$CODE_PATH/phenodigm2.py" export --db "$DB_PATH" --table "$table" | pigz -k -p4 > "$OUTPUT_DIR/${table}.tsv.gz"; then
-        echo "Processed table: $table, output saved to: $OUTPUT_DIR/${table}.tsv.gz"
+        log "Processed table: $table, output saved to: $OUTPUT_DIR/${table}.tsv.gz"
     else
-        echo "Error processing table: $table" >&2
+        log "Error processing table: $table" >&2
     fi
 done
 
 
 # 4 b. Extract the conditional disease_model_associations
-echo "Extracting conditional tables..."
+log "Extracting conditional tables..."
 conditions=("query LIKE '%OMIM%' AND match LIKE '%#%'" "query LIKE '%ORPHA%' AND match LIKE '%#%'" "query LIKE '%DECIPHER%' AND match LIKE '%#%'" "query LIKE '%OMIM%' AND match NOT LIKE '%#%'" "query LIKE '%ORPHA%' AND match NOT LIKE '%#%'" "query LIKE '%DECIPHER%' AND match NOT LIKE '%#%'")
 filenames=("disease_model_association_omim_impc" "disease_model_association_orphanet_impc" "disease_model_association_decipher_impc" "disease_model_association_omim_nonimpc" "disease_model_association_orphanet_nonimpc" "disease_model_association_decipher_nonimpc")
 
@@ -68,8 +73,8 @@ for i in "${!conditions[@]}"; do
     file="${filenames[$i]}"
 
     if python3 "$CODE_PATH/phenodigm2.py" export --db "$DB_PATH" --table disease_model_association --where "${condition}" | pigz -k -p4 > "$OUTPUT_DIR"/${file}.tsv.gz; then
-        echo "Processed file: $file, output saved to: $OUTPUT_DIR/${file}.tsv.gz"
+        log "Processed file: $file, output saved to: $OUTPUT_DIR/${file}.tsv.gz"
     else
-        echo "Error processing table: $file" >&2
+        log "Error processing table: $file" >&2
     fi
 done
