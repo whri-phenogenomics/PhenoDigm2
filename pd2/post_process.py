c7cdadbcc61dc4b9e52aff2430bb5408c8f2f7d6
dpavam <hhz161@qmul.ac.uk>
2024-11-18 16:14:01 +0000

    feat: introduce luigi for pipeline scheduling

---
+++ pd2/post_process.py
@@ -0,0 +1,98 @@
+""" Runs post processing of Phenodigm release.
+    This involves prodcing necessary files for data analysis of each DR and the Disease Models ShinyApp
+    
+    By: Diego Pava"""
+
+
+from . import tools as pd2tools
+import luigi
+from luigi import Task, LocalTarget
+import os
+from pathlib import Path
+
+
+
+# helper functions here
+def _get_post_proc_dir(config) -> Path:
+    rootdir = Path(config.db).resolve()
+    return Path(rootdir, "post_processing")
+
+def _get_pipeline_status_dir(config) -> None:
+    post_proc_dir = _get_post_proc_dir(config)
+    return Path(post_proc_dir, "pipeline_status")
+
+def run_post_process(config):
+    pd2tools.log("Running post processing pipeline")
+    
+    #LOGIC HERE
+    # post_proc_dir = _get_post_proc_dir(config)
+    # Or use default (current directory)
+    luigi.build([CreatePostProcessDirs(config=config)], local_scheduler=True)
+
+
+
+
+
+
+
+# luigi tasks here
+class CreatePostProcessDirs(Task):
+
+    config = luigi.Parameter()
+
+    def output(self):
+        post_proc_dir = _get_post_proc_dir(self.config)
+        return LocalTarget(Path(post_proc_dir, "pipeline_status",".directories_created"))
+
+    def run(self):
+        post_proc_dir = _get_post_proc_dir(self.config)
+        directories = [
+            Path(post_proc_dir, "data", "auxiliary"),
+            Path(post_proc_dir, "data", "hpo"),
+            Path(post_proc_dir, "data", "impc"),
+            Path(post_proc_dir, "data", "intermediate"),
+            Path(post_proc_dir, "data", "output"),
+            Path(post_proc_dir, "data", "phenodigm"),
+            Path(post_proc_dir, "scripts", "auxiliary"),
+        ]
+
+        try:
+            # Create all project directories
+            for dir_path in directories:
+                os.makedirs(dir_path, exist_ok=True)
+                pd2tools.log(f"Created directory: {dir_path}")
+
+            # Create the marker file to indicate successful completion
+            with self.output().open("w") as f:
+                f.write("Directories created successfully.")
+            pd2tools.log("Created post-processing directories successfully.")
+
+        except Exception as e:
+            pd2tools.log(f"Error creating directories: {e}")
+            raise e
+        
+        # Create a pipeline_status dir
+        os.makedirs(_get_pipeline_status_dir(self.config), exist_ok=True)
+
+
+class DownloadResources(Task):
+    def requires(self):
+        return CreatePostProcessDirs()
+    
+    def output(self):
+        post_proc_dir = _get_post_proc_dir(self.config)
+        return LocalTarget(Path(post_proc_dir, "pipeline_status",".files_downloaded"))
+
+    def run(self):
+        pass
+        # TODO: Files to download
+        # 1. GENTAR Orthologues: ### https://www.gentar.org/orthology-api/api/ortholog/one_to_one/impc/write_to_tsv_file
+        # 2. IMPC DR geno-pheno assertions all http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results/genotype-phenotype-assertions-ALL.csv.gz
+        # 3. IMPC viability: http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results/viability.csv.gz
+        # 4. IMPC statistical results: http://ftp.ebi.ac.uk/pub/databases/impc/all-data-releases/latest/results/statistical-results-ALL.csv.gz
+        # 5. HPO genes_to_phenotype: https://github.com/obophenotype/human-phenotype-ontology/releases/download/v2024-08-13/genes_to_phenotype.txt
+        # 6. 
+
+        # Auxiliary static
+        # 1. "./data/auxiliary/omim_curation.tsv"
+        
